f11d7f7c08f4c4d0203223e2f225d809d1d50c51
yni2 <yni2@ncsu.edu>
2015-04-09 16:41:08 -0400

    Comment out iTrustHTTPTest import see if we can build on Jenkins

--- iTrust/unittests/edu/ncsu/csc/itrust/testutils/JUnitiTrustUtils.java
+++ iTrust/unittests/edu/ncsu/csc/itrust/testutils/JUnitiTrustUtils.java
@@ -6,7 +6,7 @@ import edu.ncsu.csc.itrust.beans.DiagnosisBean;
 import edu.ncsu.csc.itrust.beans.TransactionBean;
 import edu.ncsu.csc.itrust.enums.TransactionType;
 import edu.ncsu.csc.itrust.exception.DBException;
-import edu.ncsu.csc.itrust.http.iTrustHTTPTest;
+//import edu.ncsu.csc.itrust.http.iTrustHTTPTest;
 import edu.ncsu.csc.itrust.selenium.*;
 
 @SuppressWarnings("unused")
